898f6eb54fd300307fec0cc6d65739ca34d5657b
Alex Ballera <alexballera@gmail.com>
2015-06-29 21:55:17 -0430

    host

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module BackFacturacion
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.action_mailer.default_url_options = { :host => 'https://secure-spire-9472.herokuapp.com/' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
